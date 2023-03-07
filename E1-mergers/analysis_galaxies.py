d49b526b4cfdc45d58019d99c703b474b322a084
Johannes Stoiber <Johannes.Stoiber@physik.uni-muenchen.de>
2023-03-07 23:44:41 +0100

    updated analysis

--- E1-mergers/analysis_galaxies.py
+++ E1-mergers/analysis_galaxies.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 #quants = [0.005, 0.995]
 #quants = [0.025, 0.975]
-quants = [0.05, 0.95] #<--
-n =70
-figsize1 = (10,5) #now: xlim(-38,38);(10,6)at plt.xlim(-50,50) and plt.ylim(-30,30) #default was (25,5) at plt.xlim(-100,100) and plt.ylim(-30,30)
+quants = [0.05, 0.95] 
+n = 70
+figsize1 = (8.5,5.3) 
 figsize2 = (18,6)
 figsize3 = (7.5,6)
 
@@ -56,6 +56,9 @@ class Merger():
         
         self.lambda_R = 0.5
         
+        self.semimajoraxis = (np.abs(self.x_min)+np.abs(self.x_max))/2
+        self.semiminoraxis = (np.abs(self.y_min)+np.abs(self.y_max))/2
+        
     def calc_com(self):
         self.com = np.dot(self.pos.transpose(),self.mass)/np.sum(self.mass)
         self.vel_com = np.dot(self.vel.transpose(), self.mass)/np.sum(self.mass)
@@ -75,12 +78,15 @@ class Merger():
         self.y_min = np.quantile(self.pos_bar[:,2], quants[0])
         self.y_max = np.quantile(self.pos_bar[:,2], quants[1])
         
-    def plot(self):
+        self.semimajoraxis = (np.abs(self.x_min)+np.abs(self.x_max))/2
+        self.semiminoraxis = (np.abs(self.y_min)+np.abs(self.y_max))/2
+        
+    def plot(self,show = "ellipse"):
         sort_ind = np.argsort(-self.pos_bar[:,0])
         self.pos_bar = self.pos_bar[sort_ind]
         self.vel_bar = self.vel_bar[sort_ind]
         
-        fig= plt.figure(figsize = figsize1, dpi = 80) #figsize = (25,5)
+        fig= plt.figure(figsize = figsize1, dpi = 120) #figsize = (25,5)
         plt.scatter(self.pos_dm[:,1], self.pos_dm[:,2], marker = '.',s = .5, c = 'gray')
         plt.scatter(self.pos_bar[:,1], self.pos_bar[:,2], marker = '.', c = self.vel_bar[:,0], s = .5, cmap = 'jet_r', vmin = -200, vmax = 200)
     
@@ -88,11 +94,6 @@ class Merger():
         cbar.set_label(r'$v_{los} [km/s]$')
                     
         plt.plot(0,0, 'r.')
-        
-        plt.axhline(y = self.y_min, color = 'red')
-        plt.axhline(y = self.y_max, color = 'red')
-        plt.axvline(x = self.x_min, color = 'red')
-        plt.axvline(x = self.x_max, color = 'red')
 
         #plt.xlim(-100,100)
         #plt.xlim(-50,50)
@@ -103,12 +104,26 @@ class Merger():
         plt.ylabel('x [kpc]')
     
         plt.title(f'merger at time = {self.time:.3f}')
+        
+        if show == "ellipse":
+            a = self.semimajoraxis
+            b = self.semiminoraxis
+            t = np.linspace(0,2*np.pi,100)
+            x = a*np.cos(t)
+            y = b*np.sin(t)
+            plt.plot(x,y, 'b-')
+        elif show == "quants":
+            plt.axhline(y = self.y_min, color = 'red')
+            plt.axhline(y = self.y_max, color = 'red')
+            plt.axvline(x = self.x_min, color = 'red')
+            plt.axvline(x = self.x_max, color = 'red')
+            
         plt.show()
         
     def grid(self, n_x = 50, n_y = 50):
         
-        x_edges = np.linspace(self.x_min, self.x_max, n_x+1)
-        y_edges = np.linspace(self.y_min, self.y_max, n_y+1)
+        x_edges = np.linspace(-self.semimajoraxis, self.semimajoraxis, n_x+1)
+        y_edges = np.linspace(-self.semiminoraxis, self.semiminoraxis, n_y+1)
         
         x_centers = 0.5*(x_edges[1:]+x_edges[:-1])
         y_centers = 0.5*(y_edges[1:]+y_edges[:-1]) 
@@ -119,22 +134,33 @@ class Merger():
         V_array = np.zeros((n_x, n_y), dtype = np.float64)
         sig_array = np.zeros((n_x, n_y), dtype = np.float64)
         
+        a = self.semimajoraxis
+        b = self.semiminoraxis
+        
         for i in range(len(x_centers)):
             for j in range(len(y_centers)):
                 
                 R = np.sqrt(x_centers[i]**2 + y_centers[j]**2)
                 
                 cond = np.where( (self.pos_bar[:,1] > x_edges[i]) & (self.pos_bar[:,1] < x_edges[i+1]) & (self.pos_bar[:,2] > y_edges[j]) & (self.pos_bar[:,2] < y_edges[j+1]) )
+                additional_cond = np.where( (self.pos_bar[:,1]**2/a**2 + self.pos_bar[:,2]**2/b**2 < 1) & (self.pos_bar[:,1] > x_edges[i]) & (self.pos_bar[:,1] < x_edges[i+1]) & (self.pos_bar[:,2] > y_edges[j]) & (self.pos_bar[:,2] < y_edges[j+1]) )
                 vel_bin = self.vel_bar[:,0][cond]
+                mass_bin = self.mass_bar[additional_cond]
+                vel_bin2 = self.vel_bar[:,0][additional_cond]
                 
                 V = np.mean(vel_bin)
                 sig = np.std(vel_bin)
-                
-                lam_R_enum[i,j] = R*np.abs(V)
-                lam_R_denom[i,j] = R*np.sqrt(V**2 + sig**2)
+                V2 = np.mean(vel_bin2)
+                sig2 = np.std(vel_bin2)
+                m = np.mean(mass_bin)
                 
                 V_array[i,j] = V
                 sig_array[i,j] = sig
+                
+                lam_R_enum[i,j] = m*R*np.abs(V2)
+                lam_R_denom[i,j] = m*R*np.sqrt(V2**2 + sig2**2)
+                
+                
                 
         lam_R_enum = np.nan_to_num(lam_R_enum, copy=True, nan=0.0, posinf=None, neginf=None)
         lam_R_denom = np.nan_to_num(lam_R_denom, copy=True, nan=0.0, posinf=None, neginf=None)
@@ -148,7 +174,7 @@ class Merger():
         self.los_vel = V_array
         self.los_sig = sig_array
         
-        self.lambda_R = np.sum(lam_R_enum)/np.sum(lam_R_denom)   
+        self.lambda_R = np.sum(lam_R_enum)/np.sum(lam_R_denom) 
     
     def plot_both(self):
         """
@@ -167,7 +193,7 @@ class Merger():
         #plot los velocity
         #contours_vel = axes[0].contour(x_mesh, y_mesh, z, levels = [np.quantile(z,0.393)], colors='black')
         #axes[0].clabel(contours_vel, inline=True, fontsize=8)
-        im_vel = axes[0].imshow(z, extent=[self.x_min, self.x_max, self.y_min, self.y_max], origin='lower',
+        im_vel = axes[0].imshow(z, extent=[-self.semimajoraxis, self.semimajoraxis, -self.semiminoraxis, self.semiminoraxis], origin='lower',
            cmap='jet_r', alpha=0.7, aspect="auto", vmin = np.quantile(z, 0.1), vmax = np.quantile(z,0.9))
         cbar_chi = plt.colorbar(im_vel, ax = axes[0])
         
@@ -179,7 +205,7 @@ class Merger():
         #plot los velocity dispersion
         #contours_sig = axes[1].contour(x_mesh, y_mesh, z2, levels = [np.quantile(z2,0.393)], colors='black')
         #axes[1].clabel(contours_sig, inline=True, fontsize=8)
-        im_sig = axes[1].imshow(z2, extent=[self.x_min, self.x_max, self.y_min, self.y_max], origin='lower',
+        im_sig = axes[1].imshow(z2, extent=[-self.semimajoraxis, self.semimajoraxis, -self.semiminoraxis, self.semiminoraxis], origin='lower',
            cmap='jet_r', alpha=0.7, aspect="auto", vmin = np.quantile(z2, 0.1), vmax = np.quantile(z2,0.9))
         cbar_like = plt.colorbar(im_sig, ax = axes[1])
         axes[1].set_title("l.o.s. velocity dispersion [km/s]")
@@ -188,7 +214,7 @@ class Merger():
         
         return axes
     
-    def plot_vel(self):
+    def plot_vel(self, show = 'ellipse'):
         """
         Plot the 2d l.o.s.-velocity
         """
@@ -200,7 +226,7 @@ class Merger():
         
         x_mesh, y_mesh = np.meshgrid(x,y)
         
-        im_vel = plt.imshow(z, extent=[self.x_min, self.x_max, self.y_min, self.y_max], origin='lower',
+        im_vel = plt.imshow(z, extent=[-self.semimajoraxis, self.semimajoraxis, -self.semiminoraxis, self.semiminoraxis], origin='lower',
            cmap='jet_r', alpha=0.7, aspect="auto", vmin = np.quantile(z, 0.1), vmax = np.quantile(z,0.9))
         cbar = plt.colorbar(im_vel)
         cbar.set_label(r'$v_{los} [km/s]$')
@@ -209,10 +235,21 @@ class Merger():
         plt.xlabel('y [kpc]')
         plt.ylabel('x [kpc]')
         
+        #plt.xlim(self.x_min, self.x_max)
+        #plt.ylim(self.y_min, self.y_max)
+        
+        if show == "ellipse":
+            a = self.semimajoraxis
+            b = self.semiminoraxis
+            t = np.linspace(0,2*np.pi,100)
+            x = a*np.cos(t)
+            y = b*np.sin(t)
+            plt.plot(x,y, 'k-')
+        
         plt.show()
         return 0
     
-    def plot_sig(self):
+    def plot_sig(self, show = 'ellipse'):
         """
         Plot the 2d l.o.s.-velocity dispersion
         """
@@ -224,7 +261,7 @@ class Merger():
         
         x_mesh, y_mesh = np.meshgrid(x,y)
         
-        im_vel = plt.imshow(z, extent=[self.x_min, self.x_max, self.y_min, self.y_max], origin='lower',
+        im_vel = plt.imshow(z, extent=[-self.semimajoraxis, self.semimajoraxis, -self.semiminoraxis, self.semiminoraxis], origin='lower',
            cmap='jet_r', alpha=0.7, aspect="auto", vmin = np.quantile(z, 0.1), vmax = np.quantile(z,0.9))
         cbar = plt.colorbar(im_vel)
         cbar.set_label(r'$\sigma_{los} [km/s]$')
@@ -232,6 +269,17 @@ class Merger():
         plt.title(r"l.o.s. velocity dispersion")
         plt.xlabel('y [kpc]')
         plt.ylabel('x [kpc]')
+        
+        #plt.xlim(self.x_min, self.x_max)
+        #plt.ylim(self.y_min, self.y_max)
+        
+        if show == "ellipse":
+            a = self.semimajoraxis
+            b = self.semiminoraxis
+            t = np.linspace(0,2*np.pi,100)
+            x = a*np.cos(t)
+            y = b*np.sin(t)
+            plt.plot(x,y, 'k-')
         
         plt.show()
         return 0
